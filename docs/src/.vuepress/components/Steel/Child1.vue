30d3f9d0ed42fe83f3de559958f83967e4087f64
jchatkinson <jchatkinson@gmail.com>
2020-09-04 22:13:31 -0700

    added a parent and child communicating with events

---
+++ docs/src/.vuepress/components/Steel/Child1.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p>the value is {{value}}</p>
+        <button v-on:click="$emit('test-event', cValue)">Emit an event</button>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: ['value'],
+    data () {
+      return { 
+          cValue: 2
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+  }
+</script>
---
+++ docs/src/.vuepress/components/Steel/Parent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>Parent Component</p>
+        <Steel-Child1 :value="parentvalue" v-on:test-event="logEvent($event)" />
+    </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+          parentvalue: 1.0
+      }
+    },
+    computed: {
+
+    },
+    methods: {
+        logEvent(value) {
+            console.log(`test event heard by parents v-on. The value was ${value}`)
+        }
+    }
+  }
+</script>
